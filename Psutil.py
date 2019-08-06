79f097b04c392d33d34e82c59488ecae3a3f3e46
vidushig <53723229+vidushig@users.noreply.github.com>
2019-08-06 11:23:22 +0530

    Add files via upload
    Psutil ip address and modify the dict

---
+++ Psutil.py
@@ -0,0 +1,19 @@
+import psutil
+
+addrs = psutil.net_if_addrs()
+
+port_data = []
+port_list = []
+
+for key, value in addrs.items():
+    for i in range(len(value)):
+        data = key, list(value[i])
+        port_data.append(data[0])
+        for val in data[1]:
+            port_data.append(val)
+        port_list.append(port_data)
+        port_data = []
+
+for item in port_list:
+    print(item)
+
